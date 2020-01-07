0277806df5d86945c7ddaaf4e3ba591364f6d41d
Tyler Ouellette <ouell117@uwindsor.ca>
2020-01-07 16:05:58 -0500

    prettier + eslint

--- models/Post.js
+++ models/Post.js
@@ -1,55 +1,57 @@
 const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
+
+const { Schema } = mongoose;
 
 const PostSchema = new Schema({
-  user: {
-    type: Schema.Types.ObjectId,
-    ref: 'users'
-  },
-  text: {
-    type: String,
-    required: true
-  },
-  name: {
-    type: String
-  },
-  avatar: {
-    type: String
-  },
-  likes: [
-    {
-      user: {
+    user: {
         type: Schema.Types.ObjectId,
-        ref: 'users'
-      }
-    }
-  ],
-  comments: [
-    {
-      user: {
-        type: Schema.Types.ObjectId,
-        ref: 'users'
-      },
-      text: {
+        ref: 'users',
+    },
+    text: {
         type: String,
-        required: true
-      },
-      name: {
-        type: String
-      },
-      avatar: {
-        type: String
-      },
-      date: {
+        required: true,
+    },
+    name: {
+        type: String,
+    },
+    avatar: {
+        type: String,
+    },
+    likes: [
+        {
+            user: {
+                type: Schema.Types.ObjectId,
+                ref: 'users',
+            },
+        },
+    ],
+    comments: [
+        {
+            user: {
+                type: Schema.Types.ObjectId,
+                ref: 'users',
+            },
+            text: {
+                type: String,
+                required: true,
+            },
+            name: {
+                type: String,
+            },
+            avatar: {
+                type: String,
+            },
+            date: {
+                type: Date,
+                default: Date.now,
+            },
+        },
+    ],
+    date: {
         type: Date,
-        default: Date.now
-      }
-    }
-  ],
-  date: {
-    type: Date,
-    default: Date.now
-  }
+        default: Date.now,
+    },
 });
 
+// eslint-disable-next-line no-multi-assign, no-undef
 module.exports = Post = mongoose.model('post', PostSchema);
